870e873b79b3ed41c3203a975ddeaeb9121d2eea
larry <dh821020@gmail.com>
2020-06-24 22:17:31 +0900

    import bootstrap

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div class="home">
+    {{ info }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      info:null
+    }
+  },
+  mounted(){
+   this.$http.get(`${process.env.VUE_APP_URL}/api/v1/mask`).then((response) => {
+     console.log(response.data)
+     this.info = response.data
+})
+}
+}
+</script>
