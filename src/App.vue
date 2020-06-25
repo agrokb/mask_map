1b771e54c7161ea39266693cb91caf60ae583885
larry <dh821020@gmail.com>
2020-06-26 02:45:01 +0900

    region select

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,120 @@
 <template>
-<div class="row no-gutters">
- <div class="col-sm-3">
- <div class="toolbox">
- <div class="sticky-top bg-white shadow-sm p-2">
- <div class="form-group d-flex">
- <label for="cityName" class="mr-2 col-form-label text-right">縣市</label>
- <div class="flex-fill">
- <select id="cityName" class="form-control">
- </select>
- </div>
- </div>
- <div class="form-group d-flex">
- <label for="area" class="mr-2 col-form-label text-right">地區</label>
- <div class="flex-fill">
- <select id="area" class="form-control">
- <option value="">-- Select One --</option>
- </select>
- </div>
- </div>
- <p class="mb-0 small text-muted text-right">請先選擇區域查看（綠色表示還有口罩）</p>
- </div>
- <ul class="list-group">
- <a class="list-group-item text-left">
- <h3>藥局名稱</h3>
- <p class="mb-0">
- 成人口罩：1 | 兒童口罩：2
- </p>
- <p class="mb-0">地址：<a href="https://www.google.com.tw/maps/place/..."
- target="_blank" title="Google Map">
- 地址</a>
- </p>
- </a>
- </ul>
- </div>
- </div>
- <div class="col-sm-9">
- <div id="map"></div>
- </div>
-</div>
- </template>
+    <div class="row no-gutters">
+        <div class="col-sm-3">
+            <div class="toolbox">
+                <div class="sticky-top bg-white shadow-sm p-2">
+                    <div class="form-group d-flex">
+                        <label for="cityName" class="mr-2 col-form-label text-right">縣市</label>
+                        <div class="flex-fill">
+                            <select id="cityName" class="form-control" v-model="selectedCity" @change="chooseCity">
+                                <option>{{ selectedCity }}</option>
+                                <option v-for="item in cityName" :key="item.CityEngName" v-bind:value="item.CityName">{{ item.CityName }}</option>
+                            </select>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group d-flex">
+                        <label for="area" class="mr-2 col-form-label text-right">地區</label>
+                        <div class="flex-fill">
+                            <select id="area" class="form-control">
+                                <option v-for="item in region" :key="item.AreaName">{{ item.AreaName }}</option>
+                            </select>
+                        </div>
+                    </div>
+                    <p class="mb-0 small text-muted text-right">請先選擇區域查看（綠色表示還有口罩）</p>
+                </div>
+                <ul class="list-group">
+                    <a class="list-group-item text-left" v-for="item in data" :key="item.properties.id">
+                        <h3>{{ item.properties.name }}</h3>
+                        <p class="mb-0">
+                            成人口罩：1 | 兒童口罩：2
+                        </p>
+                        <p class="mb-0">地址：
+                            <a href="https://www.google.com.tw/maps/place/..." target="_blank" title="Google Map">
+                            
+                            </a>
+                        </p>
+                    </a>
+                    <a class="list-group-item text-left">
+                        <h3>藥局名稱</h3>
+                        <p class="mb-0">
+                            成人口罩：1 | 兒童口罩：2
+                        </p>
+                        <p class="mb-0">地址：
+                            <a href="https://www.google.com.tw/maps/place/..." target="_blank" title="Google Map">
+                            
+                            </a>
+                        </p>
+                    </a>
+
+                </ul>
+            </div>
+        </div>
+        <div class="col-sm-9">
+            <div id="map"></div>
+        </div>
+    </div>
+</template>
 
 <style lang="scss">
-@import 'bootstrap/scss/bootstrap';
-#map {
- height: 100vh;
-}
-.highlight {
- background: #e9ffe3;
-}
-.toolbox {
- height: 100vh;
- overflow-y: auto;
- a {
- cursor: pointer;
- }
-}
+    @import "bootstrap/scss/bootstrap";
+
+    #map {
+        height: 100vh;
+    }
+
+    .highlight {
+        background: #e9ffe3;
+    }
+
+    .toolbox {
+        height: 100vh;
+        overflow-y: auto;
+
+        a {
+            cursor: pointer;
+        }
+    }
 </style>
 
 <script>
-import L from 'leaflet';
-import cityName from './assets/cityName.json';
+    import L from "leaflet";
+    import cityName from "./assets/cityName.json";
 
-export default {
-    name:'App',
-    components:{
-    },
-    data:()=>({
-        data:[],
-        cityName
-    }),
-    mounted(){
-        const url = 'https://protected-badlands-59246.herokuapp.com/api/v1/mask';
-        this.$http.get(url).then((response) =>{
-            console.log(response.data);
-            this.data = response.data;
-        })
-
-        let osmMap = L.map('map',{
-            center:[25.03,121.55],
-            zoom:15,
-        });
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'}).addTo(osmMap);
+    export default {
+        name: "App",
+        components: {},
+        data: () => ({
+            data: [],
+            cityName,
+            region:[],
+            selectedCity:'-- please choose --',
+            localData:[]
+        }),
+        methods: {
+            chooseCity(){
+                this.cityName.forEach((value) => {
+                    if(value.CityName == this.selectedCity){
+                        this.region = value.AreaList
+                    }
+                })
+            }
         },
-    
-}
+        mounted() {
+            const url = "https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json";
+            this.$http.get(url).then(response => {
+                console.log(response.data);
+                this.data = response.data.features;
+            });
+
+            let osmMap = L.map("map", {
+                center: [25.03, 121.55],
+                zoom: 15,
+            });
+            L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}", {
+                foo: "bar",
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+            }).addTo(osmMap);
+        },
+    };
 </script>
